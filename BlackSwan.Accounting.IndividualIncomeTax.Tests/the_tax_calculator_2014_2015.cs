7a65b396521863b05f7dddd61fb204851ad37cb5
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-09-11 00:25:47 +0800

    Added tests for 2014 to 2015 calculation

--- BlackSwan.Accounting.IndividualIncomeTax.Tests/the_tax_calculator_2014_2015.cs
+++ BlackSwan.Accounting.IndividualIncomeTax.Tests/the_tax_calculator_2014_2015.cs
@@ -28,11 +28,12 @@ namespace BlackSwan.Accounting.IndividualIncomeTax.Tests
 
             // assert
             Assert.AreEqual(18000.01m, result.TaxableIncome);
+
             Assert.AreEqual(0m, result.IncomeTax);
-            Assert.AreEqual(0m, result.MedicareLevy);
-            Assert.AreEqual(0m, result.RepairLevy);
             Assert.AreEqual(445m, result.TaxOffset);
             Assert.AreEqual(0m, result.TaxAfterOffset);
+            Assert.AreEqual(0m, result.MedicareLevy);
+            Assert.AreEqual(0m, result.RepairLevy);
             Assert.AreEqual(0m, result.TotalTaxPayable);
             Assert.AreEqual(0m, result.AverageTaxRate);
         }
@@ -49,12 +50,52 @@ namespace BlackSwan.Accounting.IndividualIncomeTax.Tests
             // assert
             Assert.AreEqual(20542m, result.TaxableIncome);
             Assert.AreEqual(444.98m, result.IncomeTax);
-            Assert.AreEqual(0m, result.MedicareLevy);
-            Assert.AreEqual(0m, result.RepairLevy);
             Assert.AreEqual(445m, result.TaxOffset);
             Assert.AreEqual(0m, result.TaxAfterOffset);
+            Assert.AreEqual(0m, result.MedicareLevy);
+            Assert.AreEqual(0m, result.RepairLevy);
             Assert.AreEqual(0m, result.TotalTaxPayable);
             Assert.AreEqual(0m, result.AverageTaxRate);
+        }
+
+        [Test]
+        public void calculate_tax_50000()
+        {
+            // arrange
+            var calculator = new Calculator();
+
+            // act
+            var result = calculator.Calculate(50000m);
+
+            // assert
+            Assert.AreEqual(50000m, result.TaxableIncome);
+            Assert.AreEqual(7797m, result.IncomeTax);
+            Assert.AreEqual(250m, result.TaxOffset);
+            Assert.AreEqual(7547m, result.TaxAfterOffset);
+            Assert.AreEqual(879.16m, result.MedicareLevy);
+            Assert.AreEqual(0m, result.RepairLevy);
+            Assert.AreEqual(8426.16m, result.TotalTaxPayable);
+            Assert.AreEqual(0.1685m, result.AverageTaxRate);
+        }
+
+        [Test]
+        public void calculate_tax_200000()
+        {
+            // arrange
+            var calculator = new Calculator();
+
+            // act
+            var result = calculator.Calculate(200000m);
+
+            // assert
+            Assert.AreEqual(200000m, result.TaxableIncome);
+            Assert.AreEqual(63547m, result.IncomeTax);
+            Assert.AreEqual(0m, result.TaxOffset);
+            Assert.AreEqual(63547m, result.TaxAfterOffset);
+            Assert.AreEqual(3879.16m, result.MedicareLevy);
+            Assert.AreEqual(400m, result.RepairLevy);
+            Assert.AreEqual(67826.16m, result.TotalTaxPayable);
+            Assert.AreEqual(0.3391m, result.AverageTaxRate);
         }
     }
 }
