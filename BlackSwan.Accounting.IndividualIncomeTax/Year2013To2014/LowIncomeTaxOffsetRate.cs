f2eb9a635e49813787614c204fd4a121ddaba383
Stephen <zeng.stephen@gmail.com>
2014-09-11 11:02:29 +0800

    Removed calculation for year 2013 to 2014

--- BlackSwan.Accounting.IndividualIncomeTax/Year2013To2014/LowIncomeTaxOffsetRate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlackSwan.Accounting.IndividualIncomeTax.Year2013To2014
-{
-    public class LowIncomeTaxOffsetRate
-    {
-        public decimal Rate { get; set; }
-        public decimal StartAmount { get; set; }
-        public decimal FullTaxOffsetAmount { get; set; }
-    }
-}
