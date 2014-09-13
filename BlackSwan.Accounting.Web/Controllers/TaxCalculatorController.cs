28e8f848b6038b88b3c9435179cc67c8e605cc25
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-09-14 01:14:07 +0800

    Web styling

---
+++ BlackSwan.Accounting.Web/Controllers/TaxCalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace BlackSwan.Accounting.Web.Controllers
+{
+    public class TaxCalculatorController : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
