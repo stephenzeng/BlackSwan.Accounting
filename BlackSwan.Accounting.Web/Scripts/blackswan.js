39e1018a2e3346a01721c014a6844a282cd6ed76
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-09-17 00:36:44 +0800

    Refactory toggle display

--- BlackSwan.Accounting.Web/Scripts/blackswan.js
+++ BlackSwan.Accounting.Web/Scripts/blackswan.js
@@ -1,9 +1,7 @@
-function toggleDisplay(divId, control) {
-    if ($(divId).hasClass('hide')) {
-        $(divId).removeClass('hide');
-        $(control).removeClass('glyphicon-plus').addClass('glyphicon-minus');
+function togglePlusIcon(controlId) {
+    if ($(controlId).hasClass('glyphicon-plus')) {
+        $(controlId).removeClass('glyphicon-plus').addClass('glyphicon-minus');
     } else {
-        $(divId).addClass('hide');
-        $(control).removeClass('glyphicon-minus').addClass('glyphicon-plus');
+        $(controlId).removeClass('glyphicon-minus').addClass('glyphicon-plus');
     }
 }
