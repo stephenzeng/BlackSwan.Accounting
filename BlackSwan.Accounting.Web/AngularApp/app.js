2999dd0cee20e21972e6995fb8f421f808ba8bb3
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-10-02 19:12:49 +0800

    Started AngularJs implementation

---
+++ BlackSwan.Accounting.Web/AngularApp/app.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('blackSwanAccounting', []);
+
+app.directive('blackSwanHeader', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'black-swan-header.html'
+    };
+});
+
+app.directive('blackSwanFooter', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'black-swan-footer.html'
+    };
+});
+
+app.directive('incomeTaxRates', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'income-tax-rates.html'
+    };
+});
